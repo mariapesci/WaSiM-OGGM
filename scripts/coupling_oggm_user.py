75b939da4c5dd66028d20867d9b555d94875a8df
mariapesci <57396432+mariapesci@users.noreply.github.com>
2023-06-28 15:48:36 +0200

    Update coupling_oggm_user.py

--- scripts/coupling_oggm_user.py
+++ scripts/coupling_oggm_user.py
@@ -76,15 +76,6 @@ cfg.PATHS['climate_file']  = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/ogg
 # Provide shapefile of the catchment
 catchment = gpd.read_file('/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm_v16/Gepatschalm/catchment_alm.shp')
 
-# # Rofenache
-# cfg.PATHS['working_dir'] = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/INCA'
-# utils.mkdir(cfg.PATHS['working_dir'], reset=False)
-# cfg.PARAMS['climate_file'] = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/monthly_meteo_inca_rofenache.nc' # adjusted through regression
-# cfg.PATHS['climate_file']  = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/monthly_meteo_inca_rofenache.nc'
-# #Provide shapefile of the catchment
-# catchment = gpd.read_file('/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/rofenache_total.shp')
-
-
 #%%
 # Definition of the glaciers that are going to be used for the runs
 # in this case, all the valid glaciers in Gepatsch for the year 2003
@@ -151,7 +142,8 @@ if adj_clim_data:
 
     funct_oggm.copy_file(path_in_def, path_in_user, rgi_ids, 'mb_calib.json')
 ####### HERE THE NEW CLIMATE_HISTORICAL.nc IS REPLACED AND USED ########
-# In[34]:
+
+#%%
 fpath = gdirs[0].get_filepath('climate_historical')
 ds = xr.open_dataset(fpath)
 # Data is in hydrological years
@@ -259,53 +251,11 @@ for gdir in gdirs:
                               store_fl_diagnostics=True);
 
 
-            # tasks.run_dynamic_spinup(gdir,
-            #                           spinup_start_yr=init_year+1,
-            #                           minimise_for='area',
-            #                           precision_percent=17.0,
-            #                           output_filesuffix='_dyn_spinup',
-            #                           ye=gdir.rgi_date+1);
-            
-            # tasks.run_with_hydro(gdir,
-            #                  run_task=tasks.run_from_climate_data,
-            #                  store_monthly_hydro=True,
-            #                  init_model_filesuffix= '_dyn_spinup',
-            #                  output_filesuffix= '_with_hydro')
-            
-            # total_run = tasks.merge_consecutive_run_outputs(gdir,
-            #                                                 input_filesuffix_1='_dyn_spinup',
-            #                                                 input_filesuffix_2='_with_hydro',
-            #                                                 output_filesuffix='')
-                           
     if init_glac == 'initialization_Eis':
         init_year = 1969
         path_init = os.path.join('/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Total_area/Initialization' , 'per_glacier')
 
-    # for gdir in gdirs:
-    #     region = gdir.rgi_id.split('.')[0]
-    #     fp = path_init+'/'+region+'/'+region+'.00'+'/'+gdir.rgi_id+'.tar.gz'
-    #     # Define the path where the "tar" files of the initialization run are stored
-    #     init_dir = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Total_area/INCA/Initialization/'
-    #     # Extract the "tar" files
-    #     with tarfile.open(fp, 'r') as tf:
-    #         fp_from_tar = tf.extractall(os.path.dirname(init_dir))
-    #     # Read the results folder (pickle) for each of the glaciers. It starts with the init_year
-    #     result_init = init_dir+gdir.rgi_id+'/result'+str(init_year)+'.pkl'
-    #     # Unpickle the results folder
-    #     with gzip.open(result_init, 'rb') as f:
-    #         result = pickle.load(f)
-    #     # Select the glacier geometry with the smallest fitness value, which will be the 
-    #     # initial geometry for the initialization in 1969.
-    #     best_fitness = result.sort_values('fitness', ascending=True)['fitness'].iloc[0]
-    #     temp_bias_init = result.sort_values('fitness', ascending=True)['temp_bias'].iloc[0]
-    #     time_init = result.sort_values('fitness', ascending=True)['time'].iloc[0]
-    #     # File with the best fit, that will be used afterwards
-    #     filename_best_fit = str(init_year)+'_past_*'+str(temp_bias_init)+'_'+str(time_init)+'.nc'
-    #     file = glob.glob(os.path.join(init_dir, gdir.rgi_id, str(init_year), 'model_geometry'+filename_best_fit))[0]
-    #     shutil.copy(file, gdir.dir)
-    #     # First run: from initialization until ???
-
-    #    Selecting best fit between simulated and observed areas at init_year
+   #    Selecting best fit between simulated and observed areas at init_year
         for gdir in gdirs:
             init_dir = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Total_area/INCA/Initialization/'
             glac_areas_1969 = pd.read_csv('/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/RGI_areas_1969.csv',
@@ -340,7 +290,6 @@ for gdir in gdirs:
         
 ds_run = utils.compile_run_output(gdirs, input_filesuffix='_historical')
 
-# In[49]:
 # ##### Plot volume and length evolution of all the glaciers:
 # time = years (in this example the total number of years was 200)
 area = []
@@ -358,14 +307,8 @@ ax2.scatter(1998, 21070000, color='r'); # 52286000 for GEP, 21260000 all Gepatsc
 ax2.scatter(gdirs[0].rgi_date, sum(area), color='g')
 ax2.scatter(2006, 20480000, color='r'); # 49650000 for GEP, 20600000 all Gepatschalm
 ax2.scatter(2015, 18600000, color='r'); # 42566000 for GEP, 18620000 all Gepatschalm
-# # Rofenache
-# ax2.scatter(1969, 23387000, color='r'); #  52237000 HEF, KAS, VER
-# ax2.scatter(1998, 21499000, color='r'); # 37740000 HEF, KAS, VER
-# ax2.scatter(gdirs[0].rgi_date, sum(area), color='g')
-# ax2.scatter(2006, 19631000, color='r'); # 32960000 HEF, KAS, VER
-# ax2.scatter(2015, 16905000, color='r'); # 27850000 HEF, KAS, VER
 
-#%%
+
 # Get mass balance for every height on the tongue for Gepatschferner:
 past_mb = massbalance.MultipleFlowlineMassBalance(gdirs[6])
 heights = [2175, 2225, 2275, 2325, 2375, 2425, 2475, 2525, 2575, 2625, 2675, 2725, 2775, 2825, 2875]
@@ -376,139 +319,35 @@ for y in years_mb:
     mb_tongue_aux.to_csv('/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm_v16/Gepatschalm/Run6/dyn_spinup_calibrated/MB_tongue/mb_'+str(y)+'.csv')
     mb_tongue.append(mb_tongue_aux)
 
-# In[50]:
-# Plot all the outputs given by OGGM for Gepatschferner:
-# years = np.arange(1970,2020)
-# for i in years:
-#     plt.rcParams["figure.figsize"] = [8,7]
-#     graphics.plot_modeloutput_map(gdirs, modelyr=i, vmax=350, filesuffix='')
-#     plt.tight_layout()
-#     plt.savefig('/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Gepatschalm/Gepatschferner/output/'+str(i)+'.png', dpi=500)
-
-# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
-# graphics.plot_inversion(gdirs, vmax=400)
-# graphics.plot_modeloutput_map(gdirs, modelyr=1998, ax=ax1, vmax=350, filesuffix='')
-# graphics.plot_modeloutput_map(gdirs, modelyr=1970, vmax=400, filesuffix='')
-# plt.tight_layout();
-#plt.savefig('/home/maria/Documents/DIRT-X_Gepatsch/oggm/glacier_thickness.png', dpi=500)
 
 #%%
-'''
-gdir=gdirs[6]
-f = gdir.get_filepath('fl_diagnostics')
-with xr.open_dataset(f) as ds:
-    # We use the "base" grouped dataset to learn about the flowlines
-    fl_ids = ds.flowlines.data
-
-# We pick the last flowline (the main one)
-with xr.open_dataset(f, group=f'fl_{fl_ids[-1]}') as ds:
-    # The data is compressed - it's a good idea to store it to memory
-    # before playing with it
-    ds = ds.load()
-ds
-# surface_m = ds.bed_h + ds.thickness_m
-# plt.fill_between(ds.dis_along_flowline, surface_m.sel(time=2004), ds.bed_h, color='C0', alpha=0.30)
-# plt.fill_between(ds.dis_along_flowline, surface_m.sel(time=2020), ds.bed_h, color='C1', alpha=0.30)
-
-# # Here we plot the glacier surface in both years and the glacier bed.
-# surface_m.sel(time=2004).plot(label='Initial glacier surface', color='C0')
-# surface_m.sel(time=2020).plot(label='Glacier surface at year 50', color='C1')
-# ds.bed_h.plot(label='glacier bed', color='k')
-
-# plt.legend(); plt.ylabel('Elevation [m]');
-
-from oggm.core.flowline import FileModel
-from shapely.ops import unary_union
-#import alphashape
-list_pol = []
-areas_gl = []
-year = 2004
-crs_out = 'EPSG:31254'
-
-smap = salem.Map(gdir.grid, countries=False, nx=gdir.grid.nx)
-with utils.ncDataset(gdir.get_filepath('gridded_data')) as nc:
-        topo = nc.variables['topo'][:]
-smap.set_topography(topo)
-model = FileModel(gdir.get_filepath('model_geometry'))
-model.run_until(year)
-crs = gdir.grid.center_grid
-clss = model.fls[::-1]
-fig = plt.figure(figsize=(6,6))
-ax = fig.add_subplot(111)
-for i, fl in zip(range(len(clss)), clss):
-        a_cur, b_cur, a_b = [], [], []
-        area_fl = fl.area_m2
-        for wi, cur, (n1, n2), ti in zip(clss[i].widths, clss[i].line.coords, 
-                          clss[i].normals, clss[i].thick):
-            
-            if ti > 0:
-                _l = shpg.LineString([shpg.Point(cur+wi/2.*n1), shpg.Point(cur+wi/2.*n2)])
-                aiw = shpg.Point(cur+wi/2.*n1)
-                biw = shpg.Point(cur+wi/2.*n2)
-                ai = salem.transform_geometry(aiw, crs=crs, to_crs=crs_out)
-                bi = salem.transform_geometry(biw, crs=crs, to_crs=crs_out)
-                point_a = (ai.x, ai.y)
-                point_b = (bi.x, bi.y)
-                a_cur.append(point_a)
-                b_cur.append(point_b)
-                a_b = a_cur+ b_cur[::-1]
-                #smap.set_geometry(ai, crs=crs_out, color='red', marker='o', markersize=2)
-                #smap.set_geometry(bi, crs=crs_out, color='red', marker='o', markersize=2)
-                #smap.set_geometry(_l, crs=crs, color='red', linewidth=0.5, zorder=40)
-                #smap.set_geometry(fl.line, crs=crs, color='blue', linewidth=0.5, zorder=40)
-            
-        #alpha = 0.95 * alphashape.optimizealpha(a_b)
-        #pol = alphashape.alphashape(a_b, alpha)
-        pol = shpg.Polygon(a_b) # A) Option to construct normal polygon from the points
-        list_pol.append(pol.buffer(0))
-        areas_gl.append(area_fl)
-        
-        
-multi_pol = unary_union(list_pol) 
-smap.set_geometry(multi_pol, crs=crs_out, edgecolor='red', linewidth=0.1, zorder=40)
-smap.plot(ax)
-
-    # Conserve area
-buffer_area = (sum(areas_gl) - multi_pol.area) / multi_pol.length
-glac_pol = multi_pol.buffer(buffer_area)
-glac_pol.area
-smap.set_geometry(glac_pol, crs=crs_out, edgecolor='seagreen', linewidth=0.2, zorder=40)
-smap.plot(ax)
-'''
-# In[52]:
-
 # From catchment widths to polygons to create the glacier outline
 # For all flowlines together
 years = np.arange(init_year+1, init_year+51) #+17
 crs_out = 'EPSG:31254'
-# path_for_shp = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/shp/outlines_shp_oggm/'
 path_for_shp = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm_v16/Gepatschalm/Run6/dyn_spinup_calibrated/shp/outlines_shp_oggm/'
  
 for gdir in gdirs:
     for year in years:
         funct_oggm.from_width_to_polygon(gdir, year, crs_out, path_for_shp, save_shp=True)
 
-# In[53]
+#%%
 # Tasks for reading glacier's polygon shp, merging them into one and get the two input grids
 # for WaSiM: glaciercells and glaciercodes.
 path_oggm_outlines = path_for_shp
 path_input_wasim = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm_v16/Gepatschalm/Run6/dyn_spinup_calibrated/shp/outlines_shp_for_wasim'
-# dem_wasim = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/dem_100m.tif'
 dem_wasim = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm_v16/Gepatschalm/dem_wasim_100m.tif'
-# grid_nodes_wasim = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/grid_nodes.shp'
 grid_nodes_wasim = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm_v16/Gepatschalm/grid.shp'
-# subbasins = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/Subbasins.shp'
 subbasins = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm_v16/Gepatschalm/subbasins.shp'
 resol_wasim = 100*100
 
-# gdirs2 = gdirs[1:] initializing in 1969: gdir[0] doesn't work, it starts from 1982
 for year in years:
     funct_oggm.prepare_grids_for_wasim(gdirs, year, crs_out, path_oggm_outlines, path_input_wasim, dem_wasim, grid_nodes_wasim, subbasins,
                                 resol_wasim)
 
 #%%
 path_shp = path_for_shp
-# path_shp = '/home/maria/Documents/DIRT-X_Gepatsch/coupling/oggm/Rofenache/ice_thickness/shp_all_glaciers/outlines_shp_oggm/'
+
 for gdir in gdirs: #[:10]: #[19:]: #[:18]: # (glacier 00783 doesn't exist after 2012 so it won't work)
     for year in years:
         # 1° create topo_smoothed and glacier_mask for each glacier
