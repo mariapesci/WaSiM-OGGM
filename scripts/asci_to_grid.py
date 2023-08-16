1a0babc78d8e8f33731e81693474f7bf65f8fef4
mariapesci <57396432+mariapesci@users.noreply.github.com>
2023-08-16 11:52:36 +0200

    Update asci_to_grid.py

--- scripts/asci_to_grid.py
+++ scripts/asci_to_grid.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 17 10:41:59 2022
+@author: María Pesci
 
-@author: pesci
+This script takes the annual grid files containing the ice thicknesses (and areas) and glacier codes
+created from OGGM's outputs and convert them into ASCII format.
+
+Needed:
+    - path: path with the glacier files for WaSIM:
+            a) 'glaciercells*.asc' (where * refers to the year), contains the glacier thickness at each
+                grid cell
+            b) 'glaciercodes*.asc' contains the glacier codes at each grid cell
+            c) the ascigrid.exe executable needs to be located in the same folder
 """
 
 import glob
@@ -13,8 +21,6 @@ path = r'E:\DIRT_X\Gepatsch\Coupling\wasim\init_states_glac_vol_oggm1.6\dyn_spin
 
 glaccells = glob.glob(path+'\glaciercells*.asc')
 glaccodes = glob.glob(path+'\glaciercodes*.asc')
-
-
 
 for glac in glaccells:
     asc_name = glac.split(path)[1].split('\\')[1]
