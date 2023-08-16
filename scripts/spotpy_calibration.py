3499582f8aa3e09deeed46454b19982ce515813c
mariapesci <57396432+mariapesci@users.noreply.github.com>
2023-08-16 14:01:04 +0200

    Update spotpy_calibration.py

--- scripts/spotpy_calibration.py
+++ scripts/spotpy_calibration.py
@@ -1,3 +1,11 @@
+"""
+@author: María Pesci
+
+This script performs the automatic calibration of WaSiM parameters, during the third run 
+of the couopling scheme. This script calls the settings defined in 'spotpy_coupling_settings.py'.
+
+"""
+
 import glob
 import os
 from datetime import datetime
@@ -7,14 +15,13 @@ import copy
 import sys
 import spotpy 
 from spotpy.parameter import Uniform
-#from spotpy_settings import wasim_model, wasim_settings
 from spotpy_coupling_settings import wasim_coupling_model, coupling_settings
+import matplotlib.pyplot as plt
 
-
+# Define the settings from 'spotpy_coupling_settings.py'
 ws = coupling_settings
 
 sys.path.append(".")
-
 
 def adjust_series(y_obs, y_sim, t_ini=None, t_fin=None):
     
@@ -77,14 +84,6 @@ class spotpy_setup():
             self.params.append(spotpy.parameter.Uniform('q0', 0.01, 1.0, optguess=1.0))
         if add_mf:        
             self.params.append(spotpy.parameter.Uniform('mf', 1.2, 4.0, optguess=2.0))
-        if add_ddf:
-            self.params.append(spotpy.parameter.Uniform('DDFi', 2., 10., optguess=4.0))
-            self.params.append(spotpy.parameter.Uniform('DDFf', 2., 8., optguess=3.5))
-            self.params.append(spotpy.parameter.Uniform('DDFs', 2., 6., optguess=2.0))
-        # if add_ddf:
-        #     self.params.append(spotpy.parameter.Uniform('ki', 1., 20., optguess=20.0))
-        #     self.params.append(spotpy.parameter.Uniform('kf', 100., 1000., optguess=500.0))
-        #     self.params.append(spotpy.parameter.Uniform('ks', 10., 100., optguess=80.0))
         if add_snow_param:
             self.params.append(spotpy.parameter.Uniform('min_slope', 0., 90., optguess=60.0))
             #self.params.append(spotpy.parameter.Uniform('fr_snow', 0., 1., optguess=0.076))
@@ -94,10 +93,7 @@ class spotpy_setup():
             self.params.append(spotpy.parameter.Uniform('k_ice', 1, 20., optguess=20.0))
             self.params.append(spotpy.parameter.Uniform('k_firn', 100., 1000., optguess=500.0))
             self.params.append(spotpy.parameter.Uniform('k_snow', 10., 100., optguess=80.0))     
-            #self.params.append(spotpy.parameter.Uniform('firn_stack', 7., 10., optguess=10.0))      
-            #self.params.append(spotpy.parameter.Uniform('rate_we', 1., 2., optguess=1.5))  
-        
-        
+       
         
         self.eval_runoff = obs_runoff
         self.eval_glmb = obs_glmb
@@ -119,12 +115,6 @@ class spotpy_setup():
         for pi, pj in zip(param_var, vector):
             print('%s = %s' % (pi, pj))   
         
-        # Running only wasim
-        # model = wasim_model(ws.working_dir, ws.ctrl_master, ws.ctrl_cal, 
-        #                                   param_var, vector, ws.sim_runoff, ws.sim_glmb, 
-        #                                   ws.target_id_runoff, ws.target_id_glac, 
-        #                                   res_include_stats=False, verbose=True)
-        
         # Running coupling model
         model = wasim_coupling_model(ws, ws.ctrl_master, ws.ctrl_cal, 
                                           param_var, vector, ws.sim_runoff, ws.sim_glmb, 
@@ -139,8 +129,7 @@ class spotpy_setup():
         
         return self.eval_runoff, self.eval_glmb
 
-    
-   
+
     def objectivefunction(self, simulation, evaluation, params=None):
  
         sim1 = self.runoff
@@ -148,6 +137,7 @@ class spotpy_setup():
         obs1 = self.eval_runoff
         obs2 = self.eval_glmb
 
+        # Adjust time series in case they have different lengths
         runoff_obs, runoff_sim = adjust_series(obs1, sim1, self.t1_cal, self.t2_cal)
         glmb_obs, glmb_sim = adjust_series(obs2, sim2, self.t1_cal, self.t2_cal)
                 
@@ -156,28 +146,25 @@ class spotpy_setup():
         obj3 = spotpy.objectivefunctions.bias(runoff_obs, runoff_sim)
         obj4 = spotpy.objectivefunctions.rsr(glmb_obs, glmb_sim)
         
-        # w1, w2, w3 = 0.63, 0.07, 0.3
-        # multi_obj = w1*(1-obj1)+(1-w1)*(1-obj2)+w2*abs(obj3)+w3*obj4
         w1, w2, w3, w4 = 0.23, 0.4, 0.07, 0.3
         multi_obj = w1*(1-obj1)+w2*(1-obj2)+w3*abs(obj3)+w4*obj4
         
-        return multi_obj # [obj1, obj2]
+        return multi_obj 
  
-
+# Run spotpy (required: observed runoff, OGGM glacier mass balance and period of calibration)
 model_run = spotpy_setup(ws.obs_runoff, ws.obs_glmb, ws.t1_cal, ws.t2_cal)
-#(working_dir, ctrl_master, ctrl_new, param_var, param_val, obs_runoff, obs_glmb, sim_runoff, sim_glmb)
 sampler = spotpy.algorithms.sceua(model_run, dbformat='csv', dbname='cal_coup_init_1969')
 sampler.sample(ws.calib_runs, ngs=7, kstop=3, peps=0.1, pcento=0.1)
+
 # ngs: number of complexes in the initial population
 # kstop: number of shuffling loops in which the criterion value must change by the given % before optimiz. is terminated
 # peps: convergence level for parameter set
 # pcento: % by which the criterion value must change in a given kstop of shuffling loops to continue optimization
 
-#results_spotpy = spotpy.analyser.load_csv_results('cal_coup_3012', usecols=0)
-# results_spotpy = np.genfromtxt(r'E:\DIRT_X\Gepatsch\Coupling\spotpy_cal\wasim_coupling\cal_coup_0301_2.csv', delimiter=',', usecols=0)
-# import matplotlib.pyplot as plt
-# fig= plt.figure(1,figsize=(9,5))
-# plt.plot(results_spotpy)#['like1'])
-# plt.show()
-# plt.ylabel('Multi objective function')
-# plt.xlabel('Iteration')
+# Read the results and plot the n° iterations vs. the objective function, to see if the model converges
+results_spotpy = np.genfromtxt(r'E:\DIRT_X\Gepatsch\Coupling\spotpy_cal\wasim_coupling\cal_coup_init_1969.csv', delimiter=',', usecols=0)
+fig= plt.figure(1,figsize=(9,5))
+plt.plot(results_spotpy)#['like1'])
+plt.show()
+plt.ylabel('Multi objective function')
+plt.xlabel('Iteration')
